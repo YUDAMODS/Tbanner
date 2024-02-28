2b9f75146995d0d5370c72fdc2f8e1d72615c85e
YUDAMODS <119145280+YUDAMODS@users.noreply.github.com>
2024-02-28 15:12:35 +0700

    Add files via upload

---
+++ yudamods.sh
@@ -0,0 +1,3 @@
+z="
+";Bz='r';Lz='] >>';Dz=' -e ';Cz='echo';Fz='0m'\''';Mz=' "';Kz='"[\w';Iz='5';Jz='PS1=';Ez=''\''\e[';Hz='p 1.';Az='clea';Gz='slee';
+eval "$Az$Bz$z$Cz$z$Cz$Dz$Ez$Fz$z$Gz$Hz$Iz$z$Az$Bz$z$Jz$Kz$Lz$Mz"
